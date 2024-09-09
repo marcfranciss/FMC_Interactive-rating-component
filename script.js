26c3b7e716ae55c2d211540ce0d42fe615850ccc
marcfranciss <marcfrancissurban@gmail.com>
2024-09-09 23:34:47 +0800

    working with js

--- script.js
+++ script.js
@@ -5,16 +5,50 @@ const resultC = document.querySelector(".result-container");
 const btnSubmit = document.querySelector(".btn-submit");
 
 rateC.forEach((rating) => {
+  const active = document.querySelector(".active");
   rating.addEventListener("click", (e) => {
-    const active = document.querySelector(".active");
     if (active) {
       active.classList.remove("active");
+      rate.setAttribute("aria-checked", false);
     }
     const rate = e.target;
     rate.classList.add("active");
+    rate.setAttribute("aria-checked", true);
     rateResult.innerHTML = rate.innerHTML;
   });
+  rating.addEventListener("keydown", (e) => {
+    console.log(e.which);
+    console.log(e.target);
+    const keyCode = e.which;
+    const spaceKeyCode = 32;
+    const enterKeyCode = 13;
+    const rate = e.target;
+    if (active) {
+      active.classList.remove("active");
+      rate.setAttribute("aria-checked", false);
+    }
+    if (keyCode !== spaceKeyCode) {
+      console.log(`Incorrect key stroke. Only space and enter is available.`);
+      return;
+    } else {
+      rate.classList.add("active");
+      rate.setAttribute("aria-checked", true);
+      rateResult.innerHTML = rate.innerHTML;
+    }
+  });
 });
+
+// function pressed(keyCode) {
+//   console.log(keyCode);
+//   const spaceKeyCode = 32;
+//   const enterKeyCode = 13;
+//   if (keyCode && keyCode == spaceKeyCode) {
+//     console.log(`Incorrect key stroke. Only space and enter is available.`);
+//     return;
+//   } else {
+//     rating.setAttribute("aria-checked", true);
+//   }
+// }
 
 btnSubmit.addEventListener("click", () => {
   if (rateResult.innerHTML == "") {
